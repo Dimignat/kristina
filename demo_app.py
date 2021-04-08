63d2711a6b83e799a008be36134a5f19048f796c
Dmitry <dimignatiev@gmail.com>
2021-04-09 02:02:49 +0300

    gif animation

--- demo_app.py
+++ demo_app.py
@@ -4,7 +4,6 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 
-
 with st.echo(code_location='below'):
     st.title("2021 World Happiness Report Dashboard")
 
@@ -48,6 +47,9 @@ with st.echo(code_location='below'):
                     line_kws={'color': 'red'})
     st.pyplot(fig)
 
+    # GIF
+    #with open('celluloid_subplots.gif')
+    st.image('celluloid_subplots.gif')
 
 
     # SECOND CHART
@@ -93,7 +95,8 @@ with st.echo(code_location='below'):
     total_mean = data.mean()
     country_mean = data[data["Country name"] == country].mean()
     output = pd.DataFrame([total_mean, country_mean], index=["Overall", country]).transpose()
-    output.drop("Healthy life expectancy", inplace=True)
+    output.drop(["Healthy life expectancy", 'Standard error of ladder score',
+                 'upperwhisker', 'lowerwhisker'], inplace=True)
 
     fig, axs = plt.subplots(1, len(output.index), figsize=(16, 8))
     fig.tight_layout()
@@ -107,57 +110,3 @@ with st.echo(code_location='below'):
         ax.set_xticklabels([r], rotation=90)
     ax.legend(["Overall", country])
     st.pyplot(fig)
-
-    # """
-    # This is a test.
-    # """
-  #   x = np.linspace(0, 10, 500)
-  #   fig = plt.figure()
-  #   plt.plot(x, np.sin(x))
-  #   plt.ylim(-2, 2)
-  #   st.pyplot(fig)
-  #
-  #   st.write("Here's our first attempt at using data to create a table:")
-  #   st.write(pd.DataFrame({
-  #   'first column': [1, 2, 3, 4],
-  #   'second column': [10, 20, 30, 40]
-  #   }))
-  #
-  #   chart_data = pd.DataFrame(
-  #    np.random.randn(20, 3),
-  #    columns=['a', 'b', 'c'])
-  #
-  #   st.line_chart(chart_data)
-  #
-  #   df = pd.DataFrame({
-  # 'first column': [1, 2, 3, 4],
-  # 'second column': [10, 20, 30, 40]
-  #   })
-  #
-  #   map_data = pd.DataFrame(
-  #   np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
-  #   columns=['lat', 'lon'])
-  #
-  #   st.map(map_data)
-  #
-  #   if st.checkbox('Show dataframe'):
-  #       chart_data = pd.DataFrame(
-  #          np.random.randn(20, 3),
-  #          columns=['a', 'b', 'c'])
-  #       chart_data
-  #
-  #   df
-  #
-  #   option = st.sidebar.selectbox(
-  #   'Which number do you like best?',
-  #    df['first column'])
-  #
-  #   'You selected:', option
-  #
-  #   left_column, right_column = st.beta_columns(2)
-  #   pressed = left_column.button('Press me?')
-  #   if pressed:
-  #       right_column.write("Woohoo!")
-  #
-  #   expander = st.beta_expander("FAQ")
-  #   expander.write("Here you could put in some really, really long explanations...")
